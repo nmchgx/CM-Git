36c7416cec2f9533e0c5c5bb49ceafb651599937
nmchgx <nmchgx@163.com>
2016-03-11 16:36:23 +0800

    【搭建项目】添加.gitignore文件，增加城市代码转换函数

---
+++ project/js/js-common.js
@@ -0,0 +1,57 @@
+function ChangeCodeToCity (code)
+{
+	switch (code)
+	{
+		case 0531:
+			return 济南;
+			break;
+		case 0532:
+			return 青岛;
+			break;
+		case 0533:
+			return 淄博;
+			break;
+		case 0534:
+			return 德州;
+			break;
+		case 0535:
+			return 烟台;
+			break;
+		case 0536:
+			return 潍坊;
+			break;
+		case 0537:
+			return 济宁;
+			break;
+		case 0538:
+			return 泰安;
+			break;
+		case 0539:
+			return 临沂;
+			break;
+		case 0530:
+			return 菏泽;
+			break;
+		case 0543:
+			return 滨州;
+			break;
+		case 0546:
+			return 东营;
+			break;
+		case 0631:
+			return 威海;
+			break;
+		case 0632:
+			return 枣庄;
+			break;
+		case 0633:
+			return 日照;
+			break;
+		case 0634:
+			return 莱芜;
+			break;
+		case 0635:
+			return 聊城;
+			break;
+	}
+}
