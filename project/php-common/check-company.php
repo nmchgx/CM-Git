feeb648e136cab5db125983bbbef89a80f18ee1a
nmchgx <nmchgx@163.com>
2016-03-22 18:49:19 +0800

    【测试】login.php, check-company.php

--- project/php-common/check-company.php
+++ project/php-common/check-company.php
@@ -1,20 +1,22 @@
 <?php
+session_start();
+
 require "sqlHelper.php";
 require "jsonHelper.php";
 
-$company_id = $_POST['company_id'];
+$company_id = $_SESSION['company_id'];
 
 if($company_id){
     $sql = "SELECT company.check FROM company WHERE company_id = '$company_id'";
     $result = $mysql->query($sql);
     $rows = mysql_num_rows( $result );
     if(rows){
-        echo "success";
+        echo "{\"msg\": \"success\"}";
     }
     else{
-        echo "error";
+        echo "{\"msg\": \"error\"}";
     }
 }
 else{
-    echo "error";
+    echo "{\"msg\": \"error\"}";
 }
