ba7cddb884c81f2ab783b9ec921b3d8f26786346
nmchgx <nmchgx@163.com>
2016-03-23 13:05:17 +0800

    【修复】check-company的bug

--- project/php-common/check-company.php
+++ project/php-common/check-company.php
@@ -4,16 +4,27 @@ session_start();
 require "sqlHelper.php";
 require "jsonHelper.php";
 
-$company_id = $_SESSION['company_id'];
-
-if($company_id){
-    $sql = "SELECT company.check FROM company WHERE company_id = '$company_id'";
-    $result = $mysql->query($sql);
-    $rows = mysql_num_rows( $result );
-    if(rows){
+if(isset($_SESSION['company_id'])){
+    $check = $_SESSION['check'];
+//    $sql = "SELECT check FROM company WHERE check = 'true' AND company_id = '$company_id'";
+//    $sqlResult = $mysql->query($sql);
+//    echo $sql;
+//    if(!empty($sqlResult)){
+//        foreach($sqlResult as $row=>$rowVal){
+//            echo $rowVal;
+//            if($rowVal == 'false') {
+//                echo "{\"msg\": \"success\"}";
+//            }
+//            else {
+//                echo "{\"msg\": \"error\"}";
+//            }
+//        }
+////        echo "{\"msg\": \"success\"}";
+//    }
+    if($check == "true") {
         echo "{\"msg\": \"success\"}";
     }
-    else{
+    else {
         echo "{\"msg\": \"error\"}";
     }
 }
