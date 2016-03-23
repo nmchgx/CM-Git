b52f16b8503cc51924e20e9c7d65a71945e8e146
VickyTse <vickyshaun36@163.com>
2016-03-23 20:58:06 +0800

    【测试php】测试php并修复bug

--- project/php-common/delete-inform.php
+++ project/php-common/delete-inform.php
@@ -5,15 +5,16 @@
  * Date: 16/3/15
  * Time: 下午9:10
  */
+require "sqlHelper.php";
+require "jsonHelper.php";
 $inform_id = $_POST['inform_id'];
 
 $sql = "DELETE FROM inform WHERE inform_id = '$inform_id'";
+$sqlResult = $mysql->query($sql);
 
-$sql_check_reason = "SELECT * From inform WHERE inform_id = '$inform_id'";
-$result = $mysql->query($sql_check_reason);
-if(empty($result)){
-    echo "success";
+if(!empty($sqlResult)){
+    echo "{\"msg\": \"success\"}";
 }
 else{
-    echo "error";
+    echo "{\"msg\": \"error\"}";
 }
