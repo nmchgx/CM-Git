12d064fb9aa1e771a631da51d262aac5ec9cab58
qiuyueshi <364482529@qq.com>
2016-03-23 19:04:50 +0800

    【提交PHP】{add-company/add-schedule/delete-company/get-all-schedule/get-schedule-data}5个PHP，未测试

--- project/php-common/add-company.php
+++ project/php-common/add-company.php
@@ -5,3 +5,24 @@
  * Date: 16/3/15
  * Time: 下午9:56
  */
+/**
+ * Write by SlarQ
+ * Data:16/3/23
+ * Time:13:57
+ */
+session_start();
+require "sqlHelper.php";
+require "jsonHelper.php";
+
+$account=$_POST['account'];
+$password=$_POST['password'];
+$city=$_POST['city'];
+//INSERT INTO table_name (列1, 列2,...) VALUES (值1, 值2,....)
+$sql="INSERT INTO company (account,paaword,city) VALUES ("+account+","+password+","+city+")";
+
+$sqlResult=$mysql->query($sql);
+if(!empty($sqlResult)){
+    echo 'success';
+}else{
+    echo'error';
+}
--- project/php-common/delete-company.php
+++ project/php-common/delete-company.php
@@ -5,3 +5,22 @@
  * Date: 16/3/15
  * Time: 下午9:57
  */
+/**
+ * Write by SlarQ
+ * Data: 16/3/23
+ * Time:12:32
+ */
+session_start();
+
+require "sqlHelper.php";
+require "jsonHelper.php";
+
+$company_id=$_SESSION['company_id'];
+    //DELETE FROM Person WHERE LastName = 'Wilson'
+$sql="DELETE FROM company WHERE company_id="+company_id;
+$sqlResult=$mysql->query($sql);
+if($sqlResult){
+    echo 'success';
+}else {
+    echo 'error';
+}
