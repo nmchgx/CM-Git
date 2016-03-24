f5ed7153c98ffa9e39246cd74ba4e5ef0e5d569f
迪力亚尔 <apple@womenzhongchuleyigepantu.local>
2016-03-24 21:36:55 +0800

    examine-data已测试，没问题

--- project/php-common/examine-data.php
+++ project/php-common/examine-data.php
@@ -14,22 +14,8 @@ require "jsonHelper.php";
 $company_id = $_POST['company_id'];
 $action = $_POST['action'];
 
-/*$sql = "SELECT company_id, status FROM data WHERE company_id = '$company_id'";
-$result = $mysql->query($sql);
-
-if(!empty($result)) {*/
-//若action为1则将status改为"已上报"
 if ($action == "1") {
-    $sql = "UPDATE data SET status = '已上报' WHERE company_id = '$company_id'";
-    $sqlResult = $mysql->query($sql);
-
-    if (!empty($sqlResult)) {
-            echo "{\"msg\": \"success\"}";
-    } else {
-            echo "{\"msg\": \"error\"}";
-    }
-} else if ($action == "2") {
-    //若action为2则将status改为"未通过"
+    //若action为1则将status改为"未通过"
     $sql = "UPDATE data SET status = '未通过' WHERE company_id = '$company_id'";
     $sqlResult = $mysql->query($sql);
 
@@ -38,8 +24,8 @@ if ($action == "1") {
     } else {
             echo "{\"msg\": \"error\"}";
     }
-} else if ($action == "3") {
-    //若action为3则将status改为"重新申报"
+} else if ($action == "2") {
+    //若action为2则将status改为"重新申报"
     $sql = "UPDATE data SET status = '重申报' WHERE company_id = '$company_id'";
     $sqlResult = $mysql->query($sql);
 
@@ -48,8 +34,8 @@ if ($action == "1") {
     } else {
         echo "{\"msg\": \"error\"}";
     }
-} else if($action == "4"){
-    //若action为4则将status改为"修改"
+} else if($action == "3"){
+    //若action为3则将status改为"修改"
     $sql = "UPDATE data SET status = '修改' WHERE company_id = '$company_id'";
     $sqlResult = $mysql->query($sql);
 
@@ -58,8 +44,8 @@ if ($action == "1") {
     } else {
         echo "{\"msg\": \"error\"}";
     }
-} else if($action == "5"){
-    //若action为5则将status改为"通过"
+} else if($action == "4"){
+    //若action为4则将status改为"通过"
     $sql = "UPDATE data SET status = '通过' WHERE company_id = '$company_id'";
     $sqlResult = $mysql->query($sql);
 
@@ -71,8 +57,3 @@ if ($action == "1") {
 } else{
     echo "{\"msg\": \"error\"}";
 }
-
-/*}
- else{
-    echo "{\"msg\": \"error\"}";
-}*/
