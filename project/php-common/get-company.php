8a9aabc50fcc9d0d904748b115fcea12d22fa80b
迪力亚尔 <apple@womenzhongchuleyigepantu.local>
2016-03-24 15:14:16 +0800

    get_company未测试，search_company修改

--- project/php-common/get-company.php
+++ project/php-common/get-company.php
@@ -1,7 +1,33 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nmchgx
- * Date: 16/3/15
- * Time: 下午9:17
+session_start();
+/* get-company
+ * created by Dilyar
+ * 查询备案/未备案企业
+ * 功能:省按照各种需求搜索查询已注册的企业
+ * 请求方式:POST
+ * Date:2016.3.24
  */
+
+require "sqlHelper.php";
+require "jsonHelper.php";
+
+$check = $_POST['check'];
+
+$sql = "SELECT company_id, account, city, area, code, company.name,
+               company.type, industry, business, contact, address, post, tel, fax,
+               email FROM company WHERE company.check = '$check'";
+
+if(!empty($sqlResult)){
+    foreach($sqlResult as $row=>$rowVal){
+            $data[$row] = $rowVal;
+    }
+
+    $result['msg'] = 'success';
+    $result['data'] = $data;
+
+    $json = JSON($result);
+    echo $json;
+}
+else{
+        echo "{\"msg\": \"error\"}";
+}
