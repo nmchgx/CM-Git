e9526d54079115632e1f246585750ab90ba9d334
qiuyueshi <364482529@qq.com>
2016-03-25 10:18:01 +0800

    【修改php】修改了5个php的错误，未测试

--- project/php-common/add-schedule.php
+++ project/php-common/add-schedule.php
@@ -18,10 +18,10 @@ $month=$_POST['month'];
 $start=$_POST['start'];
 $end=$_POST['end'];
 //INSERT INTO table_name (列1, 列2,...) VALUES (值1, 值2,....)
-$sql="INSERT INTO schedule (year,month,start,end) VALUES ("+year+","+month+","+start+","+end+")";
+$sql="INSERT INTO schedule (year,month,start,end) VALUES ("+$year+","+$month+","+$start+","+$end+")";
 $sqlResult=$mysql->query($sql);
 if(!empty($sqlResult)) {
-    echo 'success';
+    echo "{\"msg\": \"success\"}";
 }else{
-    echo'error';
+    echo "{\"msg\": \"error\"}";
 }
