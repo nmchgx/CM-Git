e03b38811a35798176c2c7766dfbd32ef2a4030f
nmchgx <nmchgx@163.com>
2016-03-28 14:43:33 +0800

    【js】完成company和province的inform

--- project/province/inform/inform.js
+++ project/province/inform/inform.js
@@ -31,24 +31,59 @@ function getInform(){
 }
 
 function fillInform (data) {
-    var inforHolder = "<div class='infor-cell'>\
-                    <h1 class='infor_title'></h1>\
-                    <p class='infor_time'></p>\
-                    <p class='infor_content'></p>\
-                </div>";
+    var inforHolder = "<div class='infor-cell' style='display: inline'>\
+                        <div class='infor_box'>\
+                            <h1 class='infor_title'></h1>\
+                            <p class='infor_time'></p>\
+                            <p class='infor_content'></p>\
+                        </div>\
+                        <button  class='btn btn-danger' type='button'>删除此通知</button>\
+                       </div>";
     var container = $('#content');
     container.empty();
+
+    var addDeleteListener = function (inform_id) {
+        return function () {
+            deleteInform (inform_id);
+        }
+    }
 
     for(var i = 0; i< data.length; i++){
         var holder = $(inforHolder);
         holder.css('display','none');
         container.append(holder);
 
-        holder.addClass('infor'+i);
-        holder.children('.infor_title').html(data[i].title);
-        holder.children('.infor_time').html(data[i].time);
-        holder.children('.infor_content').html(data[i].content);
+        holder.children('.infor_box').children('.infor_title').html(data[i].title);
+        holder.children('.infor_box').children('.infor_time').html(data[i].time);
+        holder.children('.infor_box').children('.infor_content').html(data[i].content);
+        holder.children('button').click(addDeleteListener(data[i].inform_id));
         holder.fadeIn(600);
 
     }
+}
+
+function deleteInform (inform_id) {
+    $.ajax({
+        type: "POST",
+        url: "../../php-common/delete-inform.php",
+        data:{
+            inform_id: inform_id
+        },
+        success: function (data) {
+            console.log(data.trim());
+            if (data.trim() != "") {
+                var json = JSON.parse(data.trim());
+                if (json.msg == "success") {
+                    console.log(json.data);
+                    getInform();
+                }
+                else {
+                    console.log("error: "+data);
+                }
+            }
+        },
+        error: function (data) {
+            console.log("error: "+data);
+        }
+    });
 }
