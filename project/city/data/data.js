e81b0bd04d3a109fbc6f3f8bf342462d7a41892b
VickyTse <vickyshaun36@163.com>
2016-04-05 20:43:03 +0800

    【数据审批】未测试

--- project/city/data/data.js
+++ project/city/data/data.js
@@ -1,3 +1,84 @@
 /**
  * Created by nmchgx on 16/4/5.
  */
+$(document).ready(function(){
+    getData();
+});
+
+var history_data = null;
+
+function getData(){
+    $.ajax({
+        type: "POST",
+        url: "../../php-common/get-nopass-data.php",
+        data:{
+        },
+        success: function (data) {
+            console.log(data.trim());
+            if (data.trim() != "") {
+                var json = JSON.parse(data.trim());
+                if (json.msg == "success") {
+                    console.log(json.data);
+                    fillDataList(json.data);
+                }
+                else {
+                    console.log("error: "+data);
+                }
+            }
+        },
+        error: function (data) {
+            console.log("error: "+data);
+        }
+    });
+}
+
+function fillDataList (data) {
+    history_data = data;
+
+    var listHolder = "<li>\
+                        <a href='javascript:void(0);'>\
+                        </a>\
+                      </li>";
+
+    var container = $('#list-data');
+    container.empty();
+
+    var addShowListener = function (i) {
+        return function () {
+            fillData (i);
+        }
+    }
+
+    for(var i = 0; i< data.length; i++){
+        var holder = $(listHolder);
+        holder.css('display','none');
+        container.append(holder);
+
+        holder.children('a').click(addShowListener(i));
+        if (i == 0) {
+            holder.children('a').click();
+        }
+        holder.children('a').html(history_data[i].company_id);
+        holder.fadeIn(600);
+    }
+
+
+}
+
+function fillData (i) {
+    $("#name").html(history_data[i].name);
+    $("#company_id").html(history_data[i].company_id);
+
+    $("#employment_last").html(history_data[i].employment_last);
+    $("#employment_now").html(history_data[i].employment_now);
+    $("#reason").html(history_data[i].reason);
+    $("#type").html(history_data[i].type);
+    $("#first_reason").html(history_data[i].first_reason);
+    $("#first_explain").html(history_data[i].first_explain);
+    $("#second_reason").html(history_data[i].second_reason);
+    $("#second_explain").html(history_data[i].second_explain);
+    $("#third_reason").html(history_data[i].third_reason);
+    $("#third_explain").html(history_data[i].third_explain);
+    $("#schedule_id").html(history_data[i].schedule_id);
+    $("#status").html(history_data[i].status);
+}
--- project/php-common/get-nopass-data.php
+++ project/php-common/get-nopass-data.php
@@ -9,11 +9,12 @@
 require "sqlHelper.php";
 require "jsonHelper.php";
 
-$sql = "SELECT data_id, data.time, status, company_id, employment_last, employment_now,
-               reason, data.type, first_reason, first_explain, second_reason, second_explain,
-               third_reason, third_explain, schedule_id FROM data WHERE status != '通过' or '修改'";
+$sql = "SELECT company.name,data.data_id, data.time, data.status, data.company_id, data.employment_last, data.employment_now,
+data.reason, data.type, data.first_reason, data.first_explain, data.second_reason, data.second_explain,
+               data.third_reason, data.third_explain, data.schedule_id FROM data, company WHERE data.status = '已上报' OR '重申报' AND data.compamy_id = company.company_id";
 $sqlResult = $mysql->query($sql);
 
+//echo $sql;
 if(!empty($sqlResult)){
     foreach($sqlResult as $row=>$rowVal){
         $data[$row] = $rowVal;
