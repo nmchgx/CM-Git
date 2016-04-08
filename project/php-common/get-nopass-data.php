ce2e6072dd35b5faf7fc6112022e5a7de3b27a0e
nmchgx <nmchgx@163.com>
2016-04-08 10:45:40 +0800

    【新增统计接口】

--- project/php-common/get-nopass-data.php
+++ project/php-common/get-nopass-data.php
@@ -15,7 +15,7 @@ $city = $_SESSION['city'];
 //echo $city;
 $sql = "SELECT company.name,data.data_id, data.time, data.status, data.company_id, data.employment_last, data.employment_now,
 data.reason, data.type, data.first_reason, data.first_explain, data.second_reason, data.second_explain,
-               data.third_reason, data.third_explain, data.schedule_id FROM data, company WHERE data.company_id+data.time IN (SELECT company_id+max(time) FROM data,schedule WHERE data.schedule_id = schedule.schedule_id AND start < NOW() AND end > NOW() GROUP BY company_id) AND data.company_id = company.company_id AND company.city = '$city' AND status = '已上报' OR '重申报' ORDER BY data.time DESC";
+               data.third_reason, data.third_explain, data.schedule_id FROM data, company WHERE data.company_id+data.time IN (SELECT company_id+max(time) FROM data,schedule WHERE data.schedule_id = schedule.schedule_id AND start < NOW() AND end > NOW() GROUP BY company_id) AND data.company_id = company.company_id AND company.city = '$city' AND status = '已上报' OR status = '重申报' ORDER BY data.time DESC";
 $sqlResult = $mysql->query($sql);
 
 //echo $sql;
---
+++ project/php-common/get-sum-employment.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nmchgx
+ * Date: 16/4/8
+ * Time: 上午10:43
+ */
+
+require "sqlHelper.php";
+require "jsonHelper.php";
+
+$sql = "SELECT data.schedule_id, SUM(data.employment_now), schedule.year, schedule.month AS sum FROM data JOIN schedule ON schedule.schedule_id = data.schedule_id WHERE data.company_id+data.time IN (SELECT company_id+max(time) FROM data GROUP BY company_id) AND data.status = '通过' OR  data.status = '修改通过' GROUP BY data.schedule_id ORDER BY schedule.end ASC";
+$sqlResult = $mysql->query($sql);
+$result=null;
+$data=null;
+
+if(!empty($sqlResult)){
+    foreach($sqlResult as $row=>$rowVal){
+        $data[$row]=$rowVal;
+    }
+    $result['msg']='success';
+    $result['data']=$data;
+
+    $json=JSON($result);
+    echo $json;
+}else{
+    echo "{\"msg\": \"error\"}";
+}
